c1188395547f0e58e5e8505406da881d2f278788
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-28 16:58:14 +0530

    Initial commit
    Apache License | Build with SEED™ — Umeå.

---
+++ tasks/options/jshint.js
@@ -0,0 +1,30 @@
+/*!
+ * -------------
+ * NobelDB®-Data
+ * -------------
+ * Data of NobelDB — A manually curated dedicated database of Nobel Laureates.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 28-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-contrib-jshint
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "NobelDB®-Data".
+ * ___________________________________________________________________________
+ */

98a03a830d0b271e60399b4048f761b8d77cc107
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-28 20:29:09 +0530

    Initial commit.🚩
    Help: https://nobelprize.readme.io/.

--- tasks/get.js
+++ tasks/get.js
@@ -41,3 +41,16 @@ module.exports = function(grunt) {
   var noop     = chalk.red;
   var yeep     = chalk.green;
   var okay     = chalk.blue;
+  
+  /*!
+   * Getting Started with Nobel Prize API
+   * ------------------------------------
+   * [Nobelprize.org/] offers open data to developers in two ways: An API and as Linked Data.
+   * The data is free to use and contains information about who has been awarded the Nobel Prize,
+   * when, in what prize category and the motivation, as well as basic information about the Nobel Laureates
+   * such as birth data and the affiliation at the time of the award. The data is regularly updated
+   * as the information on Nobelprize.org is updated, including at the time of announcements of new Laureates.
+   * ---------------------------------------------------------------------------------------------------------
+   * The REST based API provides different ways to list and search the data.
+   * The result is returned as JSON or CSV.
+   */
